1adea8946391a998c38b783f171a8ca2a9c7321e
andrey-kut <andpotugin@gmail.com>
2019-10-27 13:51:22 +0300

    hw2
    Задание 2 п1-3

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.sandbox;
+
+public class Distance {
+
+    public static void main(String[] args) {
+        hello("world");
+
+        Point p1 = new Point(5.5,1.2);
+        Point p2 = new Point(3.4,3.3);
+        System.out.println("Расстояние между точками равно " + p1.x + ":" + p1.y + " и " + p2.x + ":" + p2.y + " равно " + distance(p1,p2));
+    }
+
+    public static void hello (String somebody){
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double distance (Point p1, Point p2) {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
+}
